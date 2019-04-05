b6e94fc1f4b5b7050dd603100dc94ab1c1aafa05
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-04-05 19:13:58 -0400

    Add /mildly and /deeply

--- index.js
+++ index.js
@@ -34,5 +34,51 @@ bot.start(async ctx => {
         });
 });
 
+bot.command(`mildly`, async ctx => {
+    const hexCode = ctx.message.text
+        .slice(`/mildly`.length)
+        .trim();
+
+    if (!hexCode || hexCode.length !== `ffffff`.length) {
+        return ctx.reply(`Invalid hex code`);
+    }
+
+    const template = templates[0];
+    const theme = template.themeFile(hexCode);
+
+    ctx.replyWithDocument(
+        {
+            source: Buffer.from(theme, `binary`),
+            filename: `${template.name} #${hexCode}.tgx-theme`,
+        }, {
+            caption: `Theme by @tgxdark`,
+            reply_to_message_id: ctx.message.message_id,
+        }
+    );
+});
+
+bot.command(`deeply`, async ctx => {
+    const hexCode = ctx.message.text
+        .slice(`/deeply`.length)
+        .trim();
+
+    if (!hexCode || hexCode.length !== `ffffff`.length) {
+        return ctx.reply(`Invalid hex code`);
+    }
+
+    const template = templates[1];
+    const theme = template.themeFile(hexCode);
+
+    ctx.replyWithDocument(
+        {
+            source: Buffer.from(theme, `binary`),
+            filename: `${template.name} #${hexCode}.tgx-theme`,
+        }, {
+            caption: `Theme by @tgxdark`,
+            reply_to_message_id: ctx.message.message_id,
+        }
+    );
+});
+
 bot.launch();
 console.log(`Bot started...`);
