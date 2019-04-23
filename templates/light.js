b5f2c4835fdeed4cbd33f477af2f5031700d06ba
groozchique <groozchique@gmail.com>
2019-04-23 10:19:30 +0300

    templates: add template for Light theme

---
+++ templates/light.js
@@ -0,0 +1,62 @@
+module.exports = color => {
+    return `!
+name: "Mildly Light #${color} | 1.1"
+time: 1556003055
+author: "tgxdark"
+@
+lightStatusBar, parentTheme: 1
+wallpaperId: 21
+wallpaperUsageId: 2
+#
+attachContact, attachFile, attachInlineBot, attachLocation, attachPhoto, avatarRed, avatarSavedMessages, badge, bubble_buttonText, bubbleIn_textLink, bubbleOut_chatVerticalLine, bubbleOut_file, bubbleOut_inlineIcon, bubbleOut_inlineOutline, bubbleOut_inlineText, bubbleOut_messageAuthor, bubbleOut_textLink, bubbleOut_ticks, bubbleOut_ticksRead, bubbleOut_waveformActive, caption_textLink, chatListAction, chatListVerify, chatSendButton, checkActive, circleButtonNegative, circleButtonNewChat, circleButtonRegular, circleButtonTheme, controlActive, file, fileAttach, fileRed, fillingNegative, fillingPositiveContent, headerBarCallActive, headerButton, headerRemoveBackgroundHighlight, iconActive, iconNegative, inlineIcon, inlineOutline, inlineText, inputActive, introSectionActive, messageAuthor, messageVerticalLine, nameRed, notification, notificationPlayer, passcodeIcon, playerButtonActive, playerCoverPlaceholder, profileSectionActive, profileSectionActiveContent, progress, promo, seekDone, sliderActive, textLink, textNeutral, textSearchQueryHighlight, ticks, ticksRead, togglerActive, togglerNegative, waveformActive: #${color}
+avatarBlue, nameBlue: #35B7F3
+avatarCyan, nameCyan: #00ACC1
+avatarGreen, circleButtonNewSecret, nameGreen: #43A047
+avatarOrange, avatarYellow, circleButtonNewGroup, nameOrange, nameYellow: #FB8C00
+avatarPink, namePink: #F06292
+avatarViolet, nameViolet: #9A92CC
+background_icon: #7F7F7F
+background_text: #1E1E1E
+background_textLight: #727272
+badgeMuted: #E5E1E1
+badgeMutedText: #636363
+badgeText, bubble_button_noWallpaper, bubble_chatSeparator, bubble_date, bubble_date_noWallpaper, bubble_unread, bubble_unread_noWallpaper, bubbleIn_background, bubbleOut_background, chatBackground, chatKeyboard, checkContent, circleButtonChat, circleButtonOverlay, filling, headerBackground, headerButtonIcon, headerLightBackground, inlineContentActive, overlayFilling, passcode, playerCoverIcon, promoContent: #FFF
+bubble_button, chatKeyboardButton: #F2F2F2
+bubble_buttonText_noWallpaper, bubble_dateText, bubble_dateText_noWallpaper, headerIcon, headerLightIcon, headerLightText, headerTabActive, headerTabActiveText, headerText, messageSwipeContent, playerButton, unreadText: #222
+bubble_chatBackground, statusBar, statusBarLegacy: #0000
+bubble_mediaReply, bubble_mediaReply_noWallpaper: #171717
+bubble_messageSelection, bubbleIn_textLinkPressHighlight, bubbleOut_textLinkPressHighlight, bubbleOut_waveformInactive, caption_textLinkPressHighlight, fillingPressed, introSection, seekReady, sliderInactive, textLinkPressHighlight, textSelectionHighlight, togglerActiveBackground, waveformInactive: #${color}68
+bubble_messageSelectionNoWallpaper, messageSelection: #${color}28
+bubble_unreadText, bubble_unreadText_noWallpaper, bubbleIn_text, bubbleOut_text, iv_pageTitle, iv_text, passcodeText, text: #000
+bubbleIn_outline: #000000B0
+bubbleIn_progress, bubbleIn_time, bubbleOut_progress, bubbleOut_time: #868D95
+bubbleOut_outline: #000000B1
+chatListMute: #22222293
+circleButtonChatIcon, circleButtonOverlayIcon, icon: #747474
+circleButtonNewChannel: #F5AD3D
+controlContent: #FFFCFC
+controlInactive: #989898
+drawer: #00000040
+fillingPositive: #F8F8F8
+headerBarCallIncoming: #78E27E
+headerBarCallMuted: #919191
+headerRemoveBackground: #${color}AB
+headerTabInactiveText: #989595
+iconLight: #A3A3A3
+inputInactive: #3A3A3A30
+inputNegative: #F41616
+iv_linkHighlight: #26262643
+iv_preBlockBackground: #E8E6E6B7
+messageSwipeBackground: #191B1D0B
+online: #1BC620
+placeholder: #E2E2E2
+seekEmpty: #E1E1E1
+separator, shareSeparator: #00000024
+textLight: #737373
+togglerInactive: #E8E8E8
+togglerInactiveBackground: #C7C7C7
+togglerPositive: #00CD67
+togglerPositiveBackground: #00CD6768
+togglerPositiveContent: #FFF9F9
+unread: #2222220E`;
+};
