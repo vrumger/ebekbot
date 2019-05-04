5c7bb7ebb8ab4677a71152bfd93ef81df85559ac
groozchique <groozchique@gmail.com>
2019-05-04 11:50:48 +0300

    dark: I'm done

--- templates/mildly.js
+++ templates/mildly.js
@@ -10,7 +10,6 @@ avatarCyan, nameCyan: #00ACC1
 avatarGreen, circleButtonNewSecret, circleButtonPositive, fileGreen, iconPositive, nameGreen: #43A047
 avatarOrange, circleButtonNewGroup, nameOrange: #FB8C00
 avatarPink, namePink: #F06292
-avatarRed, circleButtonNegative, circleButtonNewChat, fileRed, iconNegative, nameRed: #FF5251
 avatarSavedMessages, badge, bubbleIn_textLink, bubbleOut_chatVerticalLine, bubbleOut_file, bubbleOut_inlineIcon, bubbleOut_inlineOutline, bubbleOut_inlineText, bubbleOut_messageAuthor, bubbleOut_textLink, bubbleOut_ticks, bubbleOut_ticksRead, bubbleOut_waveformActive, caption_textLink, chatListAction, chatListVerify, chatSendButton, checkActive, circleButtonRegular, circleButtonTheme, controlActive, file, fillingPositive, headerBarCallActive, headerButton, headerRemoveBackgroundHighlight, iconActive, inlineIcon, inlineOutline, inlineText, inputActive, introSectionActive, messageAuthor, messageVerticalLine, notification, notificationPlayer, online, passcodeIcon, playerButtonActive, playerCoverIcon, profileSectionActive, profileSectionActiveContent, progress, promo, seekDone, sliderActive, textLink, textNeutral, textSearchQueryHighlight, ticks, ticksRead, togglerActive, togglerNegative, waveformActive: #${color}
 avatarViolet, nameViolet: #9A92CC
 background: #0F1011
@@ -21,7 +20,7 @@ bubble_button, bubble_button_noWallpaper, bubble_date, bubble_date_noWallpaper, 
 bubble_chatBackground, chatBackground: #0D0D0F
 bubble_chatSeparator: #0D0E0FDC
 bubble_mediaReply, bubble_mediaReply_noWallpaper: #171717
-bubble_messageSelection, messageSelection: #{color}15
+bubble_messageSelection, messageSelection: #${color}15
 bubble_messageSelectionNoWallpaper: #${color}23
 bubbleIn_outline: #000000B0
 bubbleIn_textLinkPressHighlight, bubbleOut_textLinkPressHighlight, bubbleOut_waveformInactive, caption_textLinkPressHighlight, fillingPressed, introSection, seekReady, sliderInactive, textLinkPressHighlight, textSelectionHighlight, togglerActiveBackground, waveformInactive: #${color}68
@@ -33,13 +32,15 @@ controlContent: #FFFCFC
 controlInactive: #696969
 drawer: #00000040
 fileAttach: #212121
+fileRed: #E54040
 headerBarCallIncoming: #78E27E
 headerBarCallMuted: #919191
-headerRemoveBackground: #FF5252
+headerRemoveBackground: #FF5252AB
 headerTabActiveText, togglerPositiveContent: #FFF9F9
 headerTabInactiveText: #FFFFFF96
 inputNegative: #F41616
 messageSwipeBackground: #191B1D
+nameYellow: #F9C337
 placeholder: #1D1E21
 seekEmpty: #131313
 shareSeparator: #0E0E0E8E
@@ -47,4 +48,3 @@ statusBar, statusBarLegacy: #00000041
 togglerPositive: #00CD67
 togglerPositiveBackground: #00CD6768`;
 };
-
