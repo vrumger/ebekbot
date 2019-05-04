7452eaed4f45d296e8112758fc7921b801ee9b91
groozchique <groozchique@gmail.com>
2019-05-04 11:33:03 +0300

    dark: try to fix again

--- templates/mildly.js
+++ templates/mildly.js
@@ -1,24 +1,23 @@
 module.exports = color => {
     return `!
 name: "Mildly Dark #${color} | 1.2"
-time: 1556957502
 author: "tgxdark"
 @
 parentTheme: 2
 #
 avatarBlue, nameBlue: #35B7F3
 avatarCyan, nameCyan: #00ACC1
-avatarGreen, circleButtonNewSecret, circleButtonPositive, fileGreen, iconPositive, nameGreen: #43A047
-avatarOrange, circleButtonNewGroup, nameOrange: #FB8C00
+avatarGreen, nameGreen: #43A047
+avatarOrange, nameOrange: #FB8C00
 avatarPink, namePink: #F06292
-avatarRed, circleButtonNegative, circleButtonNewChat, fileRed, iconNegative, nameRed: #FF5251
-avatarSavedMessages, badge, bubbleIn_textLink, bubbleOut_chatVerticalLine, bubbleOut_file, bubbleOut_inlineIcon, bubbleOut_inlineOutline, bubbleOut_inlineText, bubbleOut_messageAuthor, bubbleOut_textLink, bubbleOut_ticks, bubbleOut_ticksRead, bubbleOut_waveformActive, caption_textLink, chatListAction, chatListVerify, chatSendButton, checkActive, circleButtonRegular, circleButtonTheme, controlActive, file, fillingPositive, headerBarCallActive, headerButton, headerRemoveBackgroundHighlight, iconActive, inlineIcon, inlineOutline, inlineText, inputActive, introSectionActive, messageAuthor, messageVerticalLine, notification, notificationPlayer, online, passcodeIcon, playerButtonActive, playerCoverIcon, profileSectionActive, profileSectionActiveContent, progress, promo, seekDone, sliderActive, textLink, textNeutral, textSearchQueryHighlight, ticks, ticksRead, togglerActive, togglerNegative, waveformActive: #${color}
 avatarViolet, nameViolet: #9A92CC
 background: #0F1011
 badgeMuted: #3B3B3B
 badgeMutedText: #CCC
-badgeText, bubble_buttonText, bubble_buttonText_noWallpaper, checkContent, circleButtonChatIcon, circleButtonOverlayIcon, inlineContentActive, promoContent: #FFF
-bubble_button, bubble_button_noWallpaper, bubble_date, bubble_date_noWallpaper, bubble_unread, bubble_unread_noWallpaper, bubbleIn_background, bubbleOut_background, chatKeyboard, filling, overlayFilling, passcode, unread: #151618
+avatarGreen, circleButtonNewSecret, circleButtonPositive, fileGreen, iconPositive, nameGreen: #43A047
+avatarSavedMessages, badge, bubbleIn_textLink, bubbleOut_chatVerticalLine, bubbleOut_file, bubbleOut_inlineIcon, bubbleOut_inlineOutline, bubbleOut_inlineText, bubbleOut_messageAuthor, bubbleOut_textLink, bubbleOut_ticks, bubbleOut_ticksRead, bubbleOut_waveformActive, caption_textLink, chatListAction, chatListVerify, chatSendButton, checkActive, circleButtonRegular, circleButtonTheme, controlActive, file, fillingPositive, headerBarCallActive, headerButton, headerRemoveBackgroundHighlight, iconActive, inlineIcon, inlineOutline, inlineText, inputActive, introSectionActive, messageAuthor, messageVerticalLine, notification, notificationPlayer, online, passcodeIcon, playerButtonActive, playerCoverIcon, profileSectionActive, profileSectionActiveContent, progress, promo, seekDone, sliderActive, textLink, textNeutral, textSearchQueryHighlight, ticks, ticksRead, togglerActive, togglerNegative, waveformActive: #${color}
+badgeText, checkContent, circleButtonChatIcon, circleButtonOverlayIcon, inlineContentActive, promoContent, playerButton, videoSliderActive: #FFF
+bubble_button, bubble_button_noWallpaper, bubble_date, bubble_date_noWallpaper, bubble_unread, bubble_unread_noWallpaper, bubbleIn_background, bubbleOut_background, chatKeyboard, filling, unread: #151618
 bubble_chatBackground, chatBackground: #0D0D0F
 bubble_chatSeparator: #0D0E0FDC
 bubble_mediaReply, bubble_mediaReply_noWallpaper: #171717
@@ -27,13 +26,16 @@ bubble_messageSelectionNoWallpaper: #${color}23
 bubbleIn_outline: #000000B0
 bubbleIn_textLinkPressHighlight, bubbleOut_textLinkPressHighlight, bubbleOut_waveformInactive, caption_textLinkPressHighlight, fillingPressed, introSection, seekReady, sliderInactive, textLinkPressHighlight, textSelectionHighlight, togglerActiveBackground, waveformInactive: #${color}68
 bubbleOut_outline: #000000B1
+bubbleOut_ticks: #FF6363
 chatKeyboardButton, headerBackground, headerLightBackground: #1A1B1E
 chatListMute, iconLight: #FFFFFFB5
+chatSeparator, separator: #000
 circleButtonChat, circleButtonOverlay, playerCoverPlaceholder: #212226
 controlContent: #FFFCFC
 controlInactive: #696969
 drawer: #00000040
 fileAttach: #212121
+fileRed: #E54040
 headerBarCallIncoming: #78E27E
 headerBarCallMuted: #919191
 headerRemoveBackground: #${color}AB
@@ -42,10 +44,13 @@ headerTabInactiveText: #FFFFFF96
 inputNegative: #F41616
 messageSwipeBackground: #191B1D
 nameYellow: #F9C337
+overlayFilling: #131415
+passcode: #151618
 placeholder: #1D1E21
 seekEmpty: #131313
 shareSeparator: #0E0E0E8E
 statusBar, statusBarLegacy: #00000041
 togglerPositive: #00CD67
-togglerPositiveBackground: #00CD676`;
+togglerPositiveBackground: #00CD6768
+videoSliderInactive: #FFF4`;
 };
